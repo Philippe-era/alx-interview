b859760dfadc51ee096d51f601b3eb4eb7ee0227
= <=>
2024-05-23 10:51:38 -0700

    STARWARS

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -7,17 +7,42 @@ const movieId = process.argv[2];
 const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 
 request(url, async (err, res, body) => {
-  err && console.log(err);
+  if (err) {
+    console.error(err);
+    return;
+  }
 
-  const charactersArray = (JSON.parse(res.body).characters);
-  for (const character of charactersArray) {
-    await new Promise((resolve, reject) => {
-      request(character, (err, res, body) => {
-        err && console.log(err);
+  if (res.statusCode !== 200) {
+    console.error('Failed to fetch movie data:', res.statusCode);
+    return;
+  }
 
-        console.log(JSON.parse(body).name);
-        resolve();
+  try {
+    const movieData = JSON.parse(body);
+    const charactersArray = movieData.characters;
+
+    for (const characterUrl of charactersArray) {
+      await new Promise((resolve, reject) => {
+        request(characterUrl, (err, res, body) => {
+          if (err) {
+            console.error(err);
+            reject(err);
+            return;
+          }
+
+          if (res.statusCode !== 200) {
+            console.error(`Failed to fetch character data (${characterUrl}):`, res.statusCode);
+            resolve();
+            return;
+          }
+
+          const characterData = JSON.parse(body);
+          console.log(characterData.name);
+          resolve();
+        });
       });
-    });
+    }
+  } catch (parseError) {
+    console.error('Error parsing JSON:', parseError);
   }
 });
