a2c1391b050492b4a0e9a4252070565919aa11c3
= <=>
2024-05-27 08:32:01 -0700

    MATRIX

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Spins the 2D matrix"""
+
+
+def rotate_2d_matrix(matrix):
+    """
+       Spins 2D matrix 90 degrees clockwise
+       Matrix is edited in-place
+       args:
+          matrix
+    """
+    left_side, right_side = 0, len(matrix) - 1
+
+    while left < right:
+        for initial in range(right_side - left_side):
+            top_side, bottom_side = left_side, right_side
+            
+            topLeft = matrix[top_side][left_side + initial]
+        
+            matrix[top_side][left_side + initial] = matrix[bottom_side - initial][left_side]
+            
+            matrix[bottom_side - initial][left_side] = matrix[bottom_side][right_side - initial]
+            
+            matrix[bottom_side][right_side - initial] = matrix[top_side + initial][right_side]
+            
+            matrix[top_side + initial][right_side] = topLeft
+            right_side -= 1
+            left_side += 1
+
