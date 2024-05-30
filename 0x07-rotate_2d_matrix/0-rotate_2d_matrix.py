8ee14c2f9f456ad43474f0fb22aa3d20d9c4db66
= <=>
2024-05-30 12:02:11 -0700

    space even

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Spins a 2D matrix"""
 
-
 def rotate_2d_matrix(matrix):
     """
        Spins 2D matrix 90 degrees clockwise
@@ -9,20 +8,21 @@ def rotate_2d_matrix(matrix):
        args:
           matrix
     """
+
     left_pos, right_pos = 0, len(matrix) - 1
 
     while left_pos < right_pos:
         for initial in range(right_pos - left_pos):
             top, bottom = left_pos, right_pos
-            
+
             topLeft = matrix[top][left_pos + initial]
-            
+
             matrix[top][left_pos + initial] = matrix[bottom - initial][left_pos]
-            
+
             matrix[bottom - initial][left_pos] = matrix[bottom][right_pos - initial]
-        
+
             matrix[bottom][right_pos - initial] = matrix[top + initial][right_pos]
-            
+
             matrix[top + initial][right_pos] = topLeft
         right_pos -= 1
         left_pos += 1
