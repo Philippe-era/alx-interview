544b06fa4c321eb7e5f13ea8652a54d1a20da528
= <=>
2024-06-05 07:39:21 -0700

    GIVES CHANGE BACK TO USER

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""A function to determine the fewest number of coins needed
+   to give to user"""
+
+
+def makeChange(coins, total):
+    """This function gives change back to the user
+    """
+    if total <= 0:
+        return 0
+
+    else:
+        coin_check = sorted(coins)
+        coin_check.reverse()
+        counter_add = 0
+        for initial in coin_check:
+            while(total >= i):
+                counter_add += 1
+                total -= initial
+        if total == 0:
+            return counter_add
+        return -1
